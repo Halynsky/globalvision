06c067609f869948972f057a023ea9b16dc28791
Andrii Martyniuk <amartyniuk@codevision.com.ua>
2017-11-22 12:45:21 +0200

    PhpMailer

--- mail.php
+++ mail.php
@@ -7,37 +7,65 @@ require 'vendor/Exception.php';
 require 'vendor/PHPMailer.php';
 require 'vendor/SMTP.php';
 
-echo 'sending';
+if(isset($_POST) && !empty($_POST)) {
+    $admin_email = 'caiterchrome@gmail.com';
+    $admin_name = 'Contact Form';
+    $reply = 'caiterchrome@gmail.com';
+    $errors = array();
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    if (!isset($name) || empty($name) || strlen($name) < 3) {
+        $errors['name'] = 'Fullname is to short or undefined';
+    }
+    if (!isset($message) || empty($message) || strlen($message) < 3) {
+        $errors['message'] = 'Message is to short or undefined';
+    }
+    if (!isset($email) || empty($email) || !validateEmail($email)) {
+        $errors['email'] = 'Email is not valid';
+    }
+    
+    if (count($errors) > 0) {
+        header('Content-type: application/json');
+        http_response_code(400);
+        echo json_encode($errors);
+    } else {
+            $content = "Name: " . $name . " \n";
+            $content .= "Email: " . $email . " \n";
+            $content .= "MEssage: " . $message . " \n";
+           
+            try {
+                $mail = new PHPMailer(true);
+                $mail->SMTPDebug = 2;
+                $mail->isSMTP();
+                $mail->Host = 'smtp.rambler.ru';
+                $mail->SMTPAuth = true;
+                $mail->Username = 'pupkin-vova@rambler.ua';
+                $mail->Password = '102030Aa';
+                $mail->SMTPSecure = 'ssl';
+                $mail->Port = 465;
+                $mail->setFrom('pupkin-vova@rambler.ua', 'GlobalVision');
+                $mail->addAddress('noliynyk@codevision.com.ua');
+                $mail->addAddress('caiterchrome@gmail.com');
+                $mail->addReplyTo('pupkin-vova@rambler.ua', 'Information');
+                $mail->isHTML(false);
+                $mail->Subject = 'Contact Form';
+                $mail->Body    = $content;
 
-
-try {
-    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-    //Server settings
-    $mail->SMTPDebug = 4;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.rambler.ru';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'pupkin-vova@rambler.ua';                 // SMTP username
-    $mail->Password = '102030Aa';                           // SMTP password
-    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 465;                                    // TCP port to connect to
-
-    //Recipients
-    $mail->setFrom('pupkin-vova@rambler.ua', 'GlobalVision');
-    $mail->addAddress('amartyniuk@codevision.com.ua', 'Joe User');     // Add a recipient
-    $mail->addAddress('noliynyk@codevision.com.ua');               // Name is optional
-    $mail->addReplyTo('pupkin-vova@rambler.ua', 'Information');
-
-    //Content
-    $mail->isHTML(false);                                  // Set email format to HTML
-    $mail->Subject = 'Subject';
-    $mail->Body    = 'qjqjqjq';
-
-    $mail->send();
-    echo 'Message has been sent';
-    // header('Content-type: application/json');
-    // http_response_code(200);
-} catch (Exception $e) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
+                $mail->send();
+                header('Content-type: application/json');
+                http_response_code(200);
+            } catch (Exception $e) {
+                http_response_code(400);
+                echo json_encode($mail->ErrorInfo);
+            }   
+        }
+} else {
+    header('Content-type: application/json');
+    http_response_code(400);
+    echo json_encode("Bad request");
+}
+function validateEmail($email)
+{
+    return preg_match('/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i', $email);
 }
