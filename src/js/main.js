5070d1133be966aff2d0c7a191c8d692c5374836
Nazariy Oliynyk <noliynyk@codevision.com.ua>
2018-01-25 17:23:20 +0200

    New design (v.0.0.5)

--- src/js/main.js
+++ src/js/main.js
@@ -185,134 +185,6 @@ $(document).ready(function () {
         }
       });
     },
-    particlesJs: function () {
-      var config = {
-        "particles": {
-          "number": {
-            "value": 70,
-            "density": {
-              "enable": true,
-              "value_area": 300
-            }
-          },
-          "color": {
-            "value": "#ffffff"
-          },
-          "shape": {
-            "type": "circle",
-            "stroke": {
-              "width": 0,
-              "color": "#ffffff"
-            },
-            "polygon": {
-              "nb_sides": 0
-            },
-            "image": {
-              "src": "img/github.svg",
-              "width": 100,
-              "height": 100
-            }
-          },
-          "opacity": {
-            "value": 0.5,
-            "random": true,
-            "anim": {
-              "enable": false,
-              "speed": 2,
-              "opacity_min": 0.2,
-              "sync": false
-            }
-          },
-          "size": {
-            "value": 1.5,
-            "random": true,
-            "anim": {
-              "enable": true,
-              "speed": 2,
-              "size_min": 1,
-              "sync": false
-            }
-          },
-          "line_linked": {
-            "enable": true,
-            "distance": 150,
-            "color": "#4c70c8",
-            "opacity": 0.4,
-            "width": 1
-          },
-          "move": {
-            "enable": true,
-            "speed": 2,
-            "direction": "none",
-            "random": true,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-              "enable": false,
-              "rotateX": 600,
-              "rotateY": 1200
-            }
-          }
-        },
-        "interactivity": {
-          "detect_on": "canvas",
-          "events": {
-            "onhover": {
-              "enable": true,
-              "mode": "grab"
-            },
-            "onclick": {
-              "enable": true,
-              "mode": "repulse"
-            },
-            "resize": true
-          },
-          "modes": {
-            "grab": {
-              "distance": 200,
-              "line_linked": {
-                "opacity": 0.7
-              }
-            },
-            "bubble": {
-              "distance": 227,
-              "size": 40,
-              "duration": 2,
-              "opacity": 0.5,
-              "speed": 3
-            },
-            "repulse": {
-              "distance": 200,
-              "duration": 0.4
-            },
-            "push": {
-              "particles_nb": 1
-            },
-            "remove": {
-              "particles_nb": 2
-            }
-          }
-        },
-        "retina_detect": true
-      };
-      var canvasParticles = $('#particles-js');
-      var width = canvasParticles.width();
-      var height = canvasParticles.height();
-      var ratio = width / height;
-      var starCount = Math.ceil(50 * ratio);
-      if (starCount >= 50) {
-        starCount = 50;
-      }
-
-      if (starCount <= 30) {
-        starCount = 30;
-      }
-      config.particles.number.value = starCount;
-
-      console.log('Particles ->' + config.particles.number.value);
-      particlesJS("particles-js", config);
-    },
     wowInit: function () {
       new WOW({
         offset: 100,
@@ -328,11 +200,9 @@ $(document).ready(function () {
   main.sendMail();
   main.moveToMouse();
   main.toggleMenu();
-  main.particlesJs();
-  main.wowInit();
+  // main.wowInit();
 
   $(window).resize(function () {
-    main.particlesJs();
-    main.wowInit();
+    // main.wowInit();
   });
 });
