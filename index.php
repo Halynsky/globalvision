7c87619e2f9e33e3a9acd8ea169d8acbee7c0dbf
Taras Halynskyi <off.bk90@gmail.com>
2018-07-23 16:58:26 +0300

    fix

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <link rel="apple-touch-icon" sizes="180x180" href="/dist/images/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/dist/images/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="/dist/images/favicon/android-chrome-192x192.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/dist/images/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/dist/images/favicon/manifest.json">
-    <link rel="mask-icon" href="/dist/images/favicon/safari-pinned-tab.svg" color="#2b5797">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="images/favicon/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="images/favicon/manifest.json">
+    <link rel="mask-icon" href="images/favicon/safari-pinned-tab.svg" color="#2b5797">
     <meta name="msapplication-TileColor" content="#2b5797">
-    <meta name="msapplication-TileImage" content="/dist/images/favicon/mstile-144x144.png">
+    <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
     <meta name="theme-color" content="#ffffff">
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -36,10 +36,10 @@ include "contacts.html";
 include "footer.html";
 ?>
 
-<script src="./bower_components/jquery/dist/jquery.min.js"></script>
-<script src="./bower_components/jquery-validation/dist/jquery.validate.min.js"></script>
-<script src="./bower_components/swiper/dist/js/swiper.min.js"></script>
-<script src="./bower_components/wow/dist/wow.min.js"></script>
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/jquery-validation/dist/jquery.validate.min.js"></script>
+<script src="bower_components/swiper/dist/js/swiper.min.js"></script>
+<script src="bower_components/wow/dist/wow.min.js"></script>
 <script src="js/main.min.js"></script>
 <script src="js/map.js"></script>
 <script
